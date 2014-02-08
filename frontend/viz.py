e98c6ee9d417de61c42005f73ba952e112e38f5c
ohnorobo <sar.laplante@gmail.com>
2014-02-07 23:59:38 -0500

    adding timing information

--- frontend/viz.py
+++ frontend/viz.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from pprint import pprint
 import sys
+import json
 
 #sys.path.append("/Users/slaplante/projects/m-viz/")
 
@@ -10,28 +11,59 @@ import echonest.remix.audio as audio
 
 
 
-def analyze(song):
+def analyze_to_json(song_audio):
+  song = []
 
-  for section in song.analysis.sections:
-    pprint(section)
+  for section in song_audio.analysis.sections:
+    section_dict = {}
+    section_dict['bars'] = []
+
+    section_dict['pitch'] = section.mean_pitches()
+    section_dict['timbre'] = section.mean_timbre()
+    section_dict['loudness'] = section.mean_loudness()
+    section_dict['start_time'] = section.start
+    section_dict['end_time'] = section.end
+
     for bar in section.children():
-      pprint("  " + str(bar))
+      bar_dict = {}
+      bar_dict['beats'] = []
+
+      bar_dict['pitch'] = bar.mean_pitches()
+      bar_dict['timbre'] = bar.mean_timbre()
+      bar_dict['loudness'] = bar.mean_loudness()
+      bar_dict['start_time'] = bar.start
+      bar_dict['end_time'] = bar.end
+
       for beat in bar.children():
-        pprint("    " + str(beat))
+        beat_dict = {}
+        beat_dict['tantums'] = []
+
+        beat_dict['pitch'] = beat.mean_pitches()
+        beat_dict['timbre'] = beat.mean_timbre()
+        beat_dict['loudness'] = beat.mean_loudness()
+        beat_dict['start_time'] = beat.start
+        beat_dict['end_time'] = beat.end
+
         for tantum in beat.children():
-          pprint("      " + str(tantum))
-          pitch = tantum.mean_pitches()
-          timbre = tantum.mean_timbre()
-          loudness = tantum.mean_loudness()
+          tantum_dict = {}
 
-          pprint("      " + str((pitch, timbre, loudness)))
+          tantum_dict['pitch'] = tantum.mean_pitches()
+          tantum_dict['timbre'] = tantum.mean_timbre()
+          tantum_dict['loudness'] = tantum.mean_loudness()
+          tantum_dict['start_time'] = tantum.start
+          tantum_dict['end_time'] = tantum.end
 
-def analyze_2(song):
-  pprint("analyzing...")
-  for section in song.analysis.sections:
-    pitch = section.pitches
-    timbre = section.timbre
-    loudness = section.loudness_max
+          beat_dict['tantums'].append(tantum_dict)
+        bar_dict['beats'].append(beat_dict)
+      section_dict['bars'].append(bar_dict)
+    song.append(section_dict)
+
+  return json.dumps(song, indent=4)
+
+
+
+
+
 
 
 
@@ -41,6 +73,5 @@ if __name__ == "__main__":
   song_filename = "../data/ketto.mp3"
   song = audio.LocalAudioFile(song_filename)
 
-  pprint(song)
-
-  analyze(song)
+  j = analyze_to_json(song)
+  print(j)
