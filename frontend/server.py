9a2cc93d9453ab7e039818d485e62f705e947393
ohnorobo <sar.laplante@gmail.com>
2014-02-08 05:57:17 -0500

    serving up a d3 bubble chart

--- frontend/server.py
+++ frontend/server.py
@@ -6,26 +6,19 @@ from glob import glob
 
 app = flask.Flask(__name__)
 
-
-
-@app.route("json/<filename>")
+@app.route("/json/<filename>")
 def get_json(filename):
-  filenames = glob("jsonfiles/*.json")
+  #filenames = glob("jsonfiles/*.json")
+  return flask.render_template("jsonfiles/" + filename)
 
-  return flask.render_template("jsonfiles/" + filename + ".json")
-
-
-
-
-@app.route("viz/<name>")
+@app.route("/viz/notes/<name>")
 def get_viz(name):
-  pass
-
+  return flask.render_template("notes.html", songjson=name+".json")
 
 
 if __name__ == "__main__":
   app.debug = True
   host="127.0.0.1"
-  port=80    #run on 80 by default
+  port=3002    #run on 80 by default
 
   app.run(host=host, port=int(port))
