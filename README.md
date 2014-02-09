8dd94476607766c770a306a93f0ae27e73f4b723
ohnorobo <sar.laplante@gmail.com>
2014-02-08 23:16:05 -0500

    setup for heroku

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ music visualization with d3 and echonest
 
 install
 
-    pip install pyechonest, remix
+    pip install pyechonest, remix, numpy, pygal, unidecode, flask
 
 https://hackpad.com/Untitled-CZPHXmj8uYa
--- frontend/server.py
+++ frontend/server.py
@@ -16,7 +16,7 @@ def normalize(array):
   mx = max(array)
   mn = min(array)
 
-  array = map(lambda x: x/mx, array)
+  #array = map(lambda x: x/mx, array)
   array = map(remove_lows, array)
   return array
 
@@ -103,5 +103,10 @@ if __name__ == "__main__":
   app.debug = True
   host="127.0.0.1"
   port=3002    #run on 80 by default
+
+  if sys.argv[1]: #run on port given from heroku
+    host = sys.argv[1]
+  if sys.argv[2]: #run on host given from heroku
+    port = sys.argv[2]
 
   app.run(host=host, port=int(port))
