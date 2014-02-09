3ecff0a0df64e73f24e0ce4f73f6994cde47523d
jgramm <James@SkinnyLove.(none)>
2014-02-09 08:32:21 -0500

    Added a rhythm measure to bars in the json. This is a measure of how well the rhythm of this bar fits in with the rhythm of a section. It is an integer in [0, tatumsPerBar], with 0 being completely uncorrelated (although now that I think about it, that would be like an echo type thing. this is like when you go to do a psychic aptitude test and you get everything wrong which is statistically impossible). Anyway, we have it now, we can visualize with it. It's 8:30 am right now. I think I'm gonna head to bed pretty soon. If you're reading this Sarah, wake me up for pre-judging. I'm probably under a table.

--- backend/import_json.py
+++ backend/import_json.py
@@ -3,29 +3,46 @@ from chord_analysis import *
 from pprint import pprint
 
 
-fileBaseName = "circle_of_life"
-json_data = open('C:/Users/James/workspace/echo/m-viz/frontend/templates/jsonfiles/' + fileBaseName + '.json')
+fileBaseName = "queen"
+json_data = open('C:/Users/James/workspace/echo/m-viz/templates/jsonfiles/' + fileBaseName + '.json')
 
 def topThree(v):
 	""" SPEED ME UP! Gets the biggest three elements of a vector and their indices"""
 	u = sorted(zip(v,range(12)))
 	
 	return u[-3:]
+
+def getMode(v):
+	myDict = {}
+	for e in v:
+		if (e in myDict):
+			myDict[e] = myDict[e] + 1
+		else: myDict[e] = 1
 	
+	s = sorted(myDict, key=myDict.get)
+	return s[-1]
+	
+def flatten(v):
+	return [item for sublist in v for item in sublist]
+
+
 
 data = json.load(json_data)
 
 
 binaryMask = [] # corresponds to accented notes
 songPitch = [0]*12
-
+accentCount = 0
+allRhythms = []
 
 for section in data:
 	sectionBinaryMask = [] # accented notes in this section
 	songPitch = [a+b for a,b in (zip(songPitch, section["pitch"]))] # add all the pitches from each section
-	
+	tatumsPerSection = []
+	sectionRhythm = [0]*12
 	
 	for bar in section["bars"]:
+		tatumsPerBar = 0
 		measureNotes = [] # the sum of the pitches from each accented tatum
 		binaryLoudnessMask = [] # possibly accented vector by loudness
 		binaryTimbreMask = [] # possibly accented vector by timbre[3]
@@ -36,6 +53,8 @@ for section in data:
 			beatBinaryMask = [] ### Binary Mask for a beak
 			
 			for tatum in beat["tatums"]:
+				tatumsPerBar += 1
+				
 				if (tatum["loudness"] > bar["loudness"]): # if this tatum's loudness is greater than the bar's loudness
 					l = 1
 				else:
@@ -74,22 +93,60 @@ for section in data:
 			
 			measureBinaryMask.append(beatBinaryMask)
 			
+			
 		sectionBinaryMask.append(measureBinaryMask)	
+		tatumsPerSection.append(tatumsPerBar)
+		pprint("sectionRhythm")
+		pprint(sectionRhythm)
+		pprint("mbm")
+		pprint(measureBinaryMask)
+		sectionRhythm = list(map(sum, zip(sectionRhythm, flatten(measureBinaryMask))))
 		
+	#pprint(tatumsPerSection)
+	# pprint(getMode(tatumsPerSection))
+	tatumCount = getMode(tatumsPerSection)
+	pprint("mode")
+	pprint(tatumCount)
+	pprint(sectionRhythm)
+	sectionRhythm = [int(round(x / (len(section["bars"])))) for x in sectionRhythm]
+	pprint(sectionRhythm)
+	pprint("done rhythms")
+	allRhythms.append(sectionRhythm)
 	binaryMask.append(sectionBinaryMask)	
 		
 # pprint(songPitch)
 # pprint(topThree(songPitch))
 # pprint(getChordType(topThree(songPitch)))
 
-f2 = open("C:/Users/James/workspace/echo/m-viz/frontend/templates/jsonfiles/newqueen.json", "w+")
+
+
+
+
+
+
+
+for i in range(len(data)):
+	
+	for j in range(len(data[i]["bars"])):
+		
+		x = sum(a*b for a,b in zip(allRhythms[i], flatten(binaryMask[i][j])))
+		pprint(allRhythms[i])
+		pprint(flatten(binaryMask[i][j]))
+		pprint(x)
+		#pprint(data[i]["bars"][j])
+		data[i]["bars"][j]["rhythm"] = x 
+
+pprint(allRhythms)
+
+		
+	
+
+f2 = open("C:/Users/James/workspace/echo/m-viz/templates/jsonfiles/newqueen.json", "w+")
 data2 = json.dumps(data, indent=4)
 f2.write(data2)
 f2.close()
 
-f.close()
-
 json_data.close()
 
-pprint(binaryMask)
+#pprint(binaryMask)
 
