d0d389c5417ed8c338b8df4bf9f66446360b361a
ohnorobo <sar.laplante@gmail.com>
2014-02-09 00:03:16 -0500

    removing numpy requirement

--- frontend/server.py
+++ frontend/server.py
@@ -4,7 +4,6 @@
 import sys, flask, os
 from glob import glob
 from pprint import pprint
-import numpy
 import pygal
 import json
 from unidecode import unidecode
@@ -16,7 +15,7 @@ def normalize(array):
   mx = max(array)
   mn = min(array)
 
-  #array = map(lambda x: x/mx, array)
+  array = map(lambda x: x/mx, array)
   array = map(remove_lows, array)
   return array
 
@@ -65,30 +64,30 @@ def get_viz(name):
 def pygal_graph(name, start, end):
   json_data = open("./templates/jsonfiles/" + name + ".json")
   data = json.load(json_data)
-  notes = numpy.matrix(numpy.zeros(12)).T
+  notes = [[],[],[],[],[],[],[],[],[],[],[],[]] # notes c to b
   times = []
 
   for section in data:
     for bar in section['bars']:
       for beat in bar['beats']:
         for tatum in beat['tantums']:
-          pitch = tatum['pitch']
-          pitch = normalize(pitch)
-          pitch = numpy.matrix(pitch).T
-          notes = numpy.hstack((notes, pitch))
+          pitches = tatum['pitch']
+          pitches = normalize(pitches)
+
+          for all_note, pitch in zip(notes, pitches):
+            all_note.append(pitch)
 
           time = tatum['start_time']
           times.append(time)
 
-  notes = notes[:,1:]
-  pprint(notes)
+  #pprint(notes)
 
   START = int(start)
   FINISH = int(end)
 
   chart = pygal.XY(stroke=False, style=RainbowStyle)
-  for i, note_data in enumerate(notes[:,START:FINISH]):
-    chart.add(note_labels[i], zip(times[START:FINISH], note_data.A1))
+  for i, note_data in enumerate(notes):
+    chart.add(note_labels[i], zip(times[START:FINISH], note_data[START:FINISH]))
 
   rendered = chart.render()
   encoded = unidecode(rendered)
